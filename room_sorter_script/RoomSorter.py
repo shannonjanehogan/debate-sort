7afbe0d63d359aa4e453f3b6559e00632b1c65c3
Rob <acksailor@gmail.com>
2017-08-10 11:31:32 -0700

    Room Sorter
    - Refactoring code to make easier for integration

--- room_sorter_script/RoomSorter.py
+++ room_sorter_script/RoomSorter.py
@@ -1,9 +1,12 @@
-from Debaters import DebaterSkill, Status, Debater
-from Teams import TeamSkill, Team
-from Rooms import Room
-from typing import Set
-import random
 import math
+import random
+from typing import Set
+
+from room_sorter_script.RoomSorterHelpers import remove_team, add_team, create_team, create_team_random, handle_proam, \
+    make_rooms_full, make_rooms_half
+from .Debaters import DebaterSkill, Status, Debater
+from .Rooms import Room
+from .Teams import TeamSkill, Team
 
 
 class RoomSorter:
@@ -58,7 +61,8 @@ class RoomSorter:
                     for g2 in self.debaters:
                         for d2 in g2:
                             if d2.name == partner_pref:
-                                self.create_team(debater, group, d2, g2)
+                                create_team(self.teams_adv, self.teams_pro, self.teams_proam, self.teams_nov, debater,
+                                            group, d2, g2)
                                 partner_found = True
                                 break
                         if partner_found:
@@ -66,19 +70,22 @@ class RoomSorter:
                     if not partner_found:
                         for d2 in self.judge_or_debate:
                             if d2.name == partner_pref:
-                                self.create_team(debater, group, d2, self.judge_or_debate)
+                                create_team(self.teams_adv, self.teams_pro, self.teams_proam, self.teams_nov, debater,
+                                            group, d2, self.judge_or_debate)
                                 break
 
     def make_teams(self):
         if self.vpi_pref == TeamSkill.PROAM:
-            self.handle_proam()
+            handle_proam(self.debaters_pro, self.debaters_nov, self.teams_adv, self.teams_pro, self.teams_proam,
+                         self.teams_nov)
         for group in self.debaters:
             while len(group) >= 2:
-                self.create_team_random(group, group)
+                create_team_random(self.teams_adv, self.teams_pro, self.teams_proam, self.teams_nov, group, group)
             # Judgment call made here that adv debaters should have priority for a pro partner
             # TODO: Confirm
             if len(self.debaters_adv) == 1 and len(self.debaters_pro) >= 1:
-                self.create_team_random(group, self.debaters_pro)
+                create_team_random(self.teams_adv, self.teams_pro, self.teams_proam, self.teams_nov, group,
+                                   self.debaters_pro)
         ironperson = self.handle_extras()
         self.maths(ironperson)
 
@@ -94,9 +101,11 @@ class RoomSorter:
                     return ironperson
         elif extras == 2:
             if len(self.debaters_adv) == 0:
-                self.create_team_random(self.debaters_pro, self.debaters_nov)
+                create_team_random(self.teams_adv, self.teams_pro, self.teams_proam, self.teams_nov, self.debaters_pro,
+                                   self.debaters_nov)
             else:
-                self.create_team_random(self.debaters_adv, self.debaters_nov)
+                create_team_random(self.teams_adv, self.teams_pro, self.teams_proam, self.teams_nov, self.debaters_adv,
+                                   self.debaters_nov)
             return None
 
     def maths(self, ironperson: Debater):
@@ -120,7 +129,7 @@ class RoomSorter:
             if to_remove == 0:
                 to_remove = 4
             for count in range(to_remove):
-                self.remove_team()
+                remove_team(self.teams_pro, self.teams_nov, self.teams_proam, self.teams_adv)
                 number_teams -= 1
                 judges_needed = math.ceil(number_teams / 4)
         while number_either > 0:
@@ -135,16 +144,18 @@ class RoomSorter:
                 d2 = random.sample(self.judge_or_debate, 1)
                 team = Team(ironperson, d2)
                 self.judge_or_debate.remove(d2)
-                self.add_team(team)
+                add_team(self.teams_adv, self.teams_pro, self.teams_proam, self.teams_nov, team)
                 number_either -= 1
                 number_teams += 1
                 for count in range(3 - extra_teams):
-                    self.create_team_random(self.judge_or_debate, self.judge_or_debate)
+                    create_team_random(self.teams_adv, self.teams_pro, self.teams_proam, self.teams_nov,
+                                       self.judge_or_debate, self.judge_or_debate)
                     number_either -= 2
                     number_teams += 1
             elif number_either >= (8 - extra_teams * 2):
                 for count in range(4 - extra_teams):
-                    self.create_team_random(self.judge_or_debate, self.judge_or_debate)
+                    create_team_random(self.teams_adv, self.teams_pro, self.teams_proam, self.teams_nov,
+                                       self.judge_or_debate, self.judge_or_debate)
                     number_either -= 2
             else:
                 for judge in self.judge_or_debate:
@@ -154,7 +165,7 @@ class RoomSorter:
                     number_either -= 1
         # Judgement call room with 2 teams better than room with 3
         if number_teams % 2 == 1:
-            self.remove_team()
+            remove_team(self.teams_pro, self.teams_nov, self.teams_proam, self.teams_adv)
             number_teams -= 1
 
     def make_rooms(self):
@@ -162,18 +173,18 @@ class RoomSorter:
             for team_group in self.teams:
                 if random.sample(team_group, 1).skill == self.vpi_pref:
                     while len(team_group) >= 4:
-                        self.make_rooms_full(team_group)
+                        make_rooms_full(self.rooms, self.judges, self.sorted_rooms, team_group)
         group_number = 0
         for team_group in self.teams:
             while len(team_group) >= 4:
-                self.make_rooms_full(team_group)
+                make_rooms_full(self.rooms, self.judges, self.sorted_rooms, team_group)
             if len(team_group) != 0 and group_number < 3:
                 for team in team_group:
                     self.teams[group_number + 1].add(team)
                     team_group.remove(team)
             # Judgement call that half rooms should be novs
             elif len(team_group) != 0 and group_number == 3:
-                self.make_rooms_half(team_group)
+                make_rooms_half(self.rooms, self.judges, self.sorted_rooms, team_group)
             group_number += 1
         while len(self.judges) > 0:
             judge = random.sample(self.judges, 1)
@@ -181,86 +192,3 @@ class RoomSorter:
             room.judges.add(judge)
             self.judges.remove(judge)
 
-    # Helper functions
-    # TODO: break out to 2nd file
-
-    def remove_team(self):
-        # Judgement call on order of removal
-        if len(self.teams_pro) > 0:
-            group = self.teams_pro
-        elif len(self.teams_nov) > 0:
-            group = self.teams_nov
-        elif len(self.teams_proam) > 0:
-            group = self.teams_proam
-        elif len(self.teams_adv) > 0:
-            group = self.teams_adv
-        team = random.sample(group, 1)
-        group.remove(team)
-
-    def add_team(self, team: Team):
-        if team.skill == TeamSkill.WORLDS:
-            group = self.teams_adv
-        elif team.skill == TeamSkill.PRO:
-            group = self.teams_pro
-        elif team.skill == TeamSkill.PROAM:
-            group = self.teams_proam
-        elif team.skill == TeamSkill.NOV:
-            group = self.teams_nov
-        if team not in group:
-            group.add(team)
-
-    def create_team(self, d1: Debater, g1: Set(Debater), d2: Debater, g2: Set(Debater)):
-        team = Team(d1, d2)
-        g1.remove(d1)
-        g2.remove(d2)
-        self.add_team(team)
-
-    def create_team_random(self, g1: Set(Debater), g2: Set(Debater)):
-        d1 = random.sample(g1, 1)
-        g1.remove(d1)
-        d2 = random.sample(g2, 1)
-        g2.remove(d2)
-        team = Team(d1, d2)
-        self.add_team(team)
-
-    def handle_proam(self):
-        if len(self.debaters_pro) < len(self.debaters_nov):
-            length = len(self.debaters_pro)
-        else:
-            length = len(self.debaters_nov)
-        for counter in range(length):
-            self.create_team_random(self.debaters_pro, self.debaters_nov)
-
-    def make_rooms_full(self, team_group: Set(Team)):
-        while len(team_group) >= 4:
-            room = random.sample(self.rooms, 1)
-            self.rooms.remove(room)
-            room.og = random.sample(team_group, 1)
-            team_group.remove(room.og)
-            room.oo = random.sample(team_group, 1)
-            team_group.remove(room.oo)
-            room.cg = random.sample(team_group, 1)
-            team_group.remove(room.cg)
-            room.co = random.sample(team_group, 1)
-            team_group.remove(room.co)
-            room.full = True
-            judge = random.sample(self.judges)
-            self.judges.remove(judge)
-            room.judges.add(judge)
-            room.calc_status()
-            self.sorted_rooms.add(room)
-
-    def make_rooms_half(self, team_group: Set(Team)):
-        while len(team_group) >= 2:
-            room = random.sample(self.rooms, 1)
-            self.rooms.remove(room)
-            room.og = random.sample(team_group, 1)
-            team_group.remove(room.og)
-            room.oo = random.sample(team_group, 1)
-            team_group.remove(room.oo)
-            room.half = True
-            judge = random.sample(self.judges)
-            self.judges.remove(judge)
-            room.judges.add(judge)
-            room.calc_status()
-            self.sorted_rooms.add(room)
---
+++ room_sorter_script/RoomSorterHelpers.py
@@ -0,0 +1,93 @@
+import random
+
+from room_sorter_script.Teams import TeamSkill, Team
+
+
+def remove_team(teams_pro, teams_nov, teams_proam, teams_adv):
+    # Judgement call on order of removal
+    if len(teams_pro) > 0:
+        group = teams_pro
+    elif len(teams_nov) > 0:
+        group = teams_nov
+    elif len(teams_proam) > 0:
+        group = teams_proam
+    elif len(teams_adv) > 0:
+        group = teams_adv
+    team = random.sample(group, 1)
+    group.remove(team)
+
+
+def add_team(teams_adv, teams_pro, teams_proam, teams_nov, team: Team):
+    if team.skill == TeamSkill.WORLDS:
+        group = teams_adv
+    elif team.skill == TeamSkill.PRO:
+        group = teams_pro
+    elif team.skill == TeamSkill.PROAM:
+        group = teams_proam
+    elif team.skill == TeamSkill.NOV:
+        group = teams_nov
+    if team not in group:
+        group.add(team)
+
+
+def create_team(teams_adv, teams_pro, teams_proam, teams_nov, d1: Debater, g1: Set(Debater), d2: Debater,
+                g2: Set(Debater)):
+    team = Team(d1, d2)
+    g1.remove(d1)
+    g2.remove(d2)
+    add_team(teams_adv, teams_pro, teams_proam, teams_nov, team)
+
+
+def create_team_random(teams_adv, teams_pro, teams_proam, teams_nov, g1: Set(Debater), g2: Set(Debater)):
+    d1 = random.sample(g1, 1)
+    g1.remove(d1)
+    d2 = random.sample(g2, 1)
+    g2.remove(d2)
+    team = Team(d1, d2)
+    add_team(teams_adv, teams_pro, teams_proam, teams_nov, team)
+
+
+def handle_proam(debaters_pro, debaters_nov, teams_adv, teams_pro, teams_proam, teams_nov):
+    if len(debaters_pro) < len(debaters_nov):
+        length = len(debaters_pro)
+    else:
+        length = len(debaters_nov)
+    for counter in range(length):
+        create_team_random(teams_adv, teams_pro, teams_proam, teams_nov, debaters_pro,
+                           debaters_nov)
+
+
+def make_rooms_full(rooms, judges, sorted_rooms, team_group: Set(Team)):
+    while len(team_group) >= 4:
+        room = random.sample(rooms, 1)
+        rooms.remove(room)
+        room.og = random.sample(team_group, 1)
+        team_group.remove(room.og)
+        room.oo = random.sample(team_group, 1)
+        team_group.remove(room.oo)
+        room.cg = random.sample(team_group, 1)
+        team_group.remove(room.cg)
+        room.co = random.sample(team_group, 1)
+        team_group.remove(room.co)
+        room.full = True
+        judge = random.sample(judges)
+        judges.remove(judge)
+        room.judges.add(judge)
+        room.calc_status()
+        sorted_rooms.add(room)
+
+
+def make_rooms_half(rooms, judges, sorted_rooms, team_group: Set(Team)):
+    while len(team_group) >= 2:
+        room = random.sample(rooms, 1)
+        rooms.remove(room)
+        room.og = random.sample(team_group, 1)
+        team_group.remove(room.og)
+        room.oo = random.sample(team_group, 1)
+        team_group.remove(room.oo)
+        room.half = True
+        judge = random.sample(judges)
+        judges.remove(judge)
+        room.judges.add(judge)
+        room.calc_status()
+        sorted_rooms.add(room)
